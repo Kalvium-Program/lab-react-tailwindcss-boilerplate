74997cbda16bfcfabd5034d0998a33baf8e4fcd1
Gouransh <147132368+Gouranshvaishnavji@users.noreply.github.com>
2024-03-06 21:47:04 +0530

    u

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
     </div>
     <footer className="bg-blue-900 text-white text-center py-6">
       <div className="mx-auto">
-        <p className="text-lg">&copy; 2024 All rights are reserved. Designed by Kritika Walia</p>
+        <p className="text-lg">&copy;Made by Kalvian! ...spread</p>
       </div>
     </footer>    </>
   )
